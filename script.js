092d4eceab6c53f67482d94e5633793b3e005e0e
Pedro H. Rosa Zara <zarapedrohenrique@gmail.com>
2024-12-12 22:35:35 -0300

    Links adicionados e alterações no design do site

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function loginSimples() {
     window.location.href = "/sites/login/index.html"
 }
 
+function insta() {
+    window.location.href = 'https://www.instagram.com/pedro_rzara/'
+}
+
+function git() {
+    window.location.href = 'https://github.com/PedrodeZara'
+}
+
+function linke() {
+    window.location.href = 'https://www.linkedin.com/in/pedro-zara-25a94b322/'
+}
+
 function vermais() {
     let alt = document.querySelector(".visu");
     let arrow = document.getElementById("img");
@@ -19,3 +31,4 @@ function vermais() {
         }
     }
 }
+
